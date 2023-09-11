bf14cd3077b5605b4e246214e211a17d8b9c08fc
Bloom <bloomtauta@gmail.com>
2023-09-11 17:01:14 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr Chocolate', 1, 'M', 'Brown', 'Short-tempered', 1),
+('Rowdy', 2, 'F', 'Black', 'Hot-tempered', 0),
+('Tabitha', 3, 'M', 'White', 'Medium-tempered', 1),
+('Seargeant Brown', 4, 'F', 'Dark-Brown', 'Short-tempered', 0),
+('Melissa', 5, 'M', 'Light-Brown', 'Medium-tempered', 1),
+('Grinch', 6, 'F', 'Black-White', 'Hot-tempered', 0),
+('Wendy', 7, 'M', 'Black', 'Short-tempered', 1),
+(NULL, 8, 'F', 'Brown', 'Medium-tempered', 0);
